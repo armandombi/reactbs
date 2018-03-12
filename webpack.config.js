a9c39315e84be8289d8967e80d9edd6ca24e6a69
Armando Bracho <abracho@overgrounddata.com>
2018-03-12 14:31:51 +0000

    setting up test

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: ["./src/js/app.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].js"
+  },
+  devServer: {
+    contentBase: "./dist"
   },
   module: {
     rules: [
@@ -14,7 +17,21 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
